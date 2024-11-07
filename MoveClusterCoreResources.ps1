6a3ba4fc6d279f3dd0890a2a1888a0014aa814fd
zscott15 <zscott15@gmail.com>
2024-11-07 15:37:50 -0500

    Delete MoveClusterCoreResources.ps1

--- MoveClusterCoreResources.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ClusterResources = Get-ClusterGroup
-$ClusterGroup = $ClusterResources | Where-Object {$_.Name -eq 'Cluster Group'}
-$HostedGroup = $ClusterResources | Where-Object {$_.Name -ne 'Cluster Group' -and $_.Name -ne 'Available Storage'}
-If($HostedGroup.OwnerNode -ne $ClusterGroup.OwnerNode)
-{
-    Move-ClusterGroup -Name $ClusterGroup.Name -Node $HostedGroup.OwnerNode -ErrorAction Stop
-}
