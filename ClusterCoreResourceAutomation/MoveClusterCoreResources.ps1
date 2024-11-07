d80470dc8c10cc79c937a5172af0b00c2b6adcae
zscott15 <zscott15@gmail.com>
2024-11-07 15:36:17 -0500

    Add files via upload

---
+++ ClusterCoreResourceAutomation/MoveClusterCoreResources.ps1
@@ -0,0 +1,7 @@
+﻿$ClusterResources = Get-ClusterGroup
+$ClusterGroup = $ClusterResources | Where-Object {$_.Name -eq 'Cluster Group'}
+$HostedGroup = $ClusterResources | Where-Object {$_.Name -ne 'Cluster Group' -and $_.Name -ne 'Available Storage'}
+If($HostedGroup.OwnerNode -ne $ClusterGroup.OwnerNode)
+{
+    Move-ClusterGroup -Name $ClusterGroup.Name -Node $HostedGroup.OwnerNode -ErrorAction Stop
+}
