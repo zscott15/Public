261f9957241b98ec0e1666a8a3fc144cd1562caa
zscott15 <zscott15@gmail.com>
2024-11-07 15:30:01 -0500

    Add files via upload
    These files help configure an automated schedule task in response to the event of a Windows Cluster Failing over.

---
+++ Register Cluster Scheduled Task.ps1
@@ -0,0 +1,12 @@
+﻿## MODIFY VARIABLE ##
+$SQLNode = 'SERVERFQDN'
+## MODIFY VARIABLE ##
+
+$Session = New-PSSession -ComputerName $SQLNode -Credential (get-t1creds)
+$Script = {
+    $Task = Get-ScheduledTask -TaskName MoveClusterCoreResources
+    $Trigger = $Task.Triggers
+    $Action = $Task.Actions
+    Register-ClusteredScheduledTask -TaskName MoveClusterCoreResources -TaskType AnyNode -Trigger $Trigger -Action $Action
+}
+Invoke-Command -Session $Session -ScriptBlock $Script
